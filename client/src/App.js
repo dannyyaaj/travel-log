255c367fe70b9da777fe2d95432acb7b469d3096
Danny Yang <dannyyaaj@gmail.com>
2020-06-07 22:51:15 -0500

    add fetch endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
+import { listLogEntries } from './API';
 
 const App = () => {
   const [viewport, setViewport] = useState({
@@ -10,9 +10,19 @@ const App = () => {
     longitude: -95.665,
     zoom: 4,
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      console.log(logEntries);
+    })()
+  }, [])
+
+
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle = "mapbox://styles/dannyyaaj/ckb5vtheq3a6n1io35nnkp9hu"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} 
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
